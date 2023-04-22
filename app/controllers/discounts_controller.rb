2e34e467ced0394b7c735b42b2f4f650dac0509b
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-21 23:06:10 -0400

    feat: add new and create actions to Discount_controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -3,4 +3,25 @@ class DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @discount = Discount.new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @discount = Discount.new(discount_params)
+    if @discount.save
+      redirect_to(merchant_discounts_path(params[:merchant_id]))
+    else
+      flash[:error] = "Fill in all fields."
+      @merchant = Merchant.find(params[:merchant_id])
+      render :new
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:percent, :threshold, :merchant_id)
+  end
 end
