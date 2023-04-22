3a6e2b1e13e53f33e4231544dc7da7cc2008396c
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-21 23:07:37 -0400

    test: add tests for Discount validations

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Discount, type: :model do
     it { should belong_to :merchant }
     it { should have_many(:items).through(:merchant) }
   end
+
+  describe "validations" do
+    it {should validate_presence_of :threshold}
+    it {should validate_presence_of :percent}
+  end
 end
