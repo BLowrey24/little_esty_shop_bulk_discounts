b0d65e1aa27f2b97d1781935efd92b6ec9323c30
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-23 17:14:55 -0400

    feat: add edit and update action to the discounts_controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -31,6 +31,20 @@ class DiscountsController < ApplicationController
     redirect_to(merchant_discounts_path(params[:merchant_id]))
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    if @discount.update(discount_params)
+      redirect_to(merchant_discount_path(@discount.merchant.id, @discount.id))
+    else
+      flash[:error] = "All fields must be filled above 0 and below 100."
+      render :edit
+    end
+  end
+
   private
   def discount_params
     params.permit(:percent, :threshold, :merchant_id)
