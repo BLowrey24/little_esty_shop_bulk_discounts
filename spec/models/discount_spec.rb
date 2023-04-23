9a15f68abc499af3818091fd7626713fe17baae5
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-23 17:18:38 -0400

    test: add commented out test in the discount model spec will hopefully figure it out later

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
+  # before(:each) do
+  #   @merchant = Merchant.create!(name: "Merchant", status: 0)
+
+  #   @discount_1 = @merchant.discounts.create!(percent: 10, threshold: 0)
+  #   @discount_2 = @merchant.discounts.create!(percent: 0, threshold: 10)
+  # end
   describe 'relationships' do
     it { should belong_to :merchant }
     it { should have_many(:items).through(:merchant) }
@@ -9,5 +15,9 @@ RSpec.describe Discount, type: :model do
   describe "validations" do
     it {should validate_presence_of :threshold}
     it {should validate_presence_of :percent}
+    # it 'should validate the percent attribute and the threshold attribute' do
+    #   expect(@discount_1).to_not be_valid
+    #   expect(@discount_2).to_not be_valid
+    # end
   end
 end
