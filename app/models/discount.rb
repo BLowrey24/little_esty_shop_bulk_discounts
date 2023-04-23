fa0186327acfa5ef11b408394d7d6be89450f900
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-23 17:15:33 -0400

    feat: add more validations to the discounts model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,5 +2,7 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
 
+  validates :threshold, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100}
+  validates :percent, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100}
   validates_presence_of :percent, :threshold
 end
