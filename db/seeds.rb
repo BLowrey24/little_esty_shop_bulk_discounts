1ad646fe1eb16f586ccae5f659e577736a8d0c21
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-25 22:47:42 -0400

    refactor: update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 require 'faker'
 
-Customer.destroy_all
-Merchant.destroy_all
-Invoice.destroy_all
+Discount.destroy_all
 InvoiceItem.destroy_all
 Item.destroy_all
 Transaction.destroy_all
-Discount.destroy_all
+Invoice.destroy_all
+Customer.destroy_all
+Merchant.destroy_all
 
 # merchants
   2.times do
